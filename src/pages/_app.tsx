cad21f90d67d373bf6d1938f73bbd2eee7c68a90
niedermansam <niederman.sam@gmail.com>
2023-04-02 14:04:22 -0600

    add umami analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import Script from "next/script";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Script
+        src="https://umami-sepia.vercel.app/umami.js"
+        data-website-id="ef3e6196-b4b5-4103-8bc2-68db63433e81"
+        id="umami-script"
+        async
+        defer
+      />
       <Component {...pageProps} />
     </SessionProvider>
   );
