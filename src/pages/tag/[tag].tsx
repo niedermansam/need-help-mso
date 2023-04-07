668d3eb9a6048ba4893856ac7b9ef118faa52dec
niedermansam <niederman.sam@gmail.com>
2023-04-07 11:56:50 -0600

    update loading styles

--- src/pages/tag/[tag].tsx
+++ src/pages/tag/[tag].tsx
@@ -19,29 +19,29 @@ export default function TagPage({
   return (
     <div className="text-stone-700">
       <NavBar />
-      <h1 className="mx-6 mb-4 pt-20 text-3xl font-extrabold capitalize">
+      <h1 className="mx-6 mb-4 pt-20 text-3xl font-extrabold capitalize text-stone-500">
         Tag: {decodeTag(tag)}
       </h1>
       <div className="mx-6">
-        <h2 className=" -mb-4 text-2xl font-extrabold text-stone-500">
+        <h2 className=" -mb-2 text-2xl font-extrabold text-stone-600">
           Resources
         </h2>
         {data &&
           data?.resources.map((resource) => {
             return <ResourceCard resource={resource} key={resource.id} />;
           })}
-        {isLoading && <LoadingAnimation />}
-        {isError && <div>{error.message || "Sorry, something went wrong"}</div>}
+        {isLoading && <div className="h-20 flex items-center"> <LoadingAnimation /> </div>}
+        {isError && <div><p>{error.message || "Sorry, something went wrong"}</p></div>}
       </div>
       <div className="mx-6">
-        <h2 className="-mb-3 text-2xl font-extrabold text-stone-500">
+        <h2 className="-mb-2 text-2xl font-extrabold text-stone-600">
           Organizations
         </h2>
         {data &&
           data?.organizations.map((org) => {
             return <OrganizationCard org={org} key={org.id} admin={false} />;
           })}
-        {isLoading && <LoadingAnimation />}
+        {isLoading && <div className="h-20 flex items-center"> <LoadingAnimation /> </div>}
         {isError && <div>{error.message || "Sorry, something went wrong"}</div>}
       </div>
     </div>
