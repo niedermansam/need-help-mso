dfe11d39e305926519d98726a2d82c4dd4b39a0e
Sam Supplee-Niederman <niederman.sam@gmail.com>
2023-09-13 16:57:04 -0600

    force cache reset?

--- src/app/api/org/route.ts
+++ src/app/api/org/route.ts
@@ -1,8 +1,10 @@
 import { prisma } from "@/server/db";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: Request) {
   // const cat = request.nextUrl.searchParams.get('cat')
+
+  console.log(req.body)
 
   const orgs = await prisma.organization.findMany({
     include: {
