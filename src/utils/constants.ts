57ba4fb5a56ec2bc18edca6890d617b0e8c84645
Sam Supplee-Niederman <niederman.sam@gmail.com>
2023-09-13 17:45:58 -0600

    fix caching bug?

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-import { env } from "@/env.mjs";
+//import { env } from "@/env.mjs";
 
-export const SITE_URL = env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://www.needhelpmissoula.org";
+export const SITE_URL = "https://www.needhelpmissoula.org";
 // export const SITE_URL = "https://www.needhelpmissoula.org";
