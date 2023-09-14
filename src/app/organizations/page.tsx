a58a355c8998311378e87b88ab042db7abb947ff
Sam Supplee-Niederman <niederman.sam@gmail.com>
2023-09-13 18:25:33 -0600

    switch to trpc

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -4,32 +4,29 @@ import React from 'react'
 // import type { Organization } from '@prisma/client'
 import { OrganizationCard } from '@/components/DisplayCard'
 import { prisma } from '@/server/db'
+import {appRouter } from '@/server/api/root'
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/server/auth';
 
 async function OrganizationPage() {
 
-
-    const orgs = await prisma.organization.findMany({
-        include: {
-            tags: {
-                select: {
-                    tag: true
-                }
-            }
-        }
-
-    })
+      const session = await getServerSession(authOptions)
+      const trpc = appRouter.createCaller({
+         prisma,
+        session,
+      });
 
 
-    console.log(orgs)
+    const orgs = await trpc.organization.getAll()
 
-    const admin = false;
-    const favoritesList:string[] = []
-    const loggedIn = false;
+    const admin = session?.user.admin || false;
+    const favoritesList = await trpc.user.getFavoriteList()
+    const loggedIn = !!session?.user;
 
   return (
     <div>
         <h1>Organizations</h1>
-        {orgs.map( org => <OrganizationCard key={org.id} org={org} admin={admin}  favoriteIds={favoritesList} loggedIn={loggedIn} />)}
+        {orgs.map( org => <OrganizationCard key={org.id} org={org} admin={admin}  favoriteIds={favoritesList.organizations} loggedIn={loggedIn} />)}
     </div>
   )
 }
