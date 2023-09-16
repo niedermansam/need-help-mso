8bfde363dbf34796d819804f74ebbab5adc7b21f
Sam Supplee-Niederman <niederman.sam@gmail.com>
2023-09-16 15:49:23 -0600

    statically generate pages from db

--- src/app/_components/organization/CreateForm.tsx
+++ src/app/_components/organization/CreateForm.tsx
@@ -15,25 +15,44 @@ import { twMerge } from "tailwind-merge";
 
 export const NEW_ORG_URL = "/admin/org/new";
 
+const DEFAULT_CITY = "Missoula"
+const DEFAULT_STATE = "MT"
+const DEFAULT_ZIP = "59801"
+
+const DEFAULT_OBJECT = {
+  city: DEFAULT_CITY,
+  state: DEFAULT_STATE,
+  zip: DEFAULT_ZIP,
+} as const
+
 export function NewOrganizationForm() {
-  const createOrganization = api.organization.create.useMutation();
+  const createOrganization = api.organization.create.useMutation({
+    onSuccess: () => {
+      alert("Organization created successfully!");
+      setFormData({ ...DEFAULT_OBJECT, category: formData?.category  });
+    }
+
+  });
 
   type CreateOrg = Parameters<typeof createOrganization.mutate>[0];
 
-  const [formData, setFormData] = React.useState<Partial<CreateOrg>>();
+  const [formData, setFormData] = React.useState<Partial<CreateOrg>>(DEFAULT_OBJECT);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!formData) return;
 
-    const { category, name, description } = formData;
+    const { category, name, description, email } = formData;
+
+    const sanitizedEmail = email === "" ? undefined : email;
+
 
     if (category === undefined || category === null) return;
     if (name === undefined || name === null) return;
 
     if (description === undefined || description === null) return;
 
-    createOrganization.mutate({ ...formData, category, name, description });
+    createOrganization.mutate({ ...formData, category, name, description, email: sanitizedEmail });
   };
 
   const handleChange = (
@@ -45,7 +64,7 @@ export function NewOrganizationForm() {
 
   return (
     <>
-      <h1 className="pb-3 text-2xl ">Edit Organization</h1>
+      <h1 className="pb-3 text-2xl ">Add Organization</h1>
       <form
         className="grid-rows-auto grid grid-cols-1 gap-x-6 md:grid-cols-4"
         onSubmit={handleSubmit}
@@ -63,9 +82,11 @@ export function NewOrganizationForm() {
             name="name"
             id="name"
             onChange={handleChange}
+            value={formData?.name || ""}
           />
         </FormItemWrapper>
-        <FormItemWrapper className=" min-h-[170px] md:row-span-4">
+
+        <FormItemWrapper className=" min-h-[170px] md:row-span-4 ">
           <label
             className="text-sm font-light lowercase text-stone-600 "
             htmlFor="description"
@@ -77,52 +98,8 @@ export function NewOrganizationForm() {
             name="description"
             id="description"
             onChange={handleChange}
+            value={formData?.description || ""}
           ></textarea>
-        </FormItemWrapper>
-        <FormItemWrapper>
-          <label
-            className="text-sm font-light lowercase text-stone-600"
-            htmlFor="phone"
-          >
-            Phone
-          </label>
-          <input
-            type="text"
-            name="phone"
-            id="phone"
-            className="text-bold rounded border border-stone-200 p-2 "
-            onChange={handleChange}
-          />
-        </FormItemWrapper>
-        <FormItemWrapper>
-          <label
-            className="text-sm font-light lowercase text-stone-600"
-            htmlFor="email"
-          >
-            Email
-          </label>
-
-          <input
-            type="text"
-            name="email"
-            id="email"
-            className="text-bold rounded border border-stone-200 p-2 "
-            onChange={handleChange}
-          />
-        </FormItemWrapper>
-        <FormItemWrapper>
-          <label
-            className="text-sm font-light lowercase text-stone-600"
-            htmlFor="website"
-          >
-            Website
-          </label>
-          <input
-            type="text"
-            name="website"
-            className="text-bold rounded border border-stone-200 p-2 "
-            id="website"
-          />
         </FormItemWrapper>
         <FormItemWrapper>
           <label
@@ -146,6 +123,121 @@ export function NewOrganizationForm() {
                 : []
             }
           />
+        </FormItemWrapper>
+        <FormItemWrapper className="md:col-span-1">
+          <label
+            className="text-sm font-light lowercase text-stone-600"
+            htmlFor="phone"
+          >
+            Phone
+          </label>
+          <input
+            type="text"
+            name="phone"
+            id="phone"
+            className="text-bold rounded border border-stone-200 p-2 "
+            onChange={handleChange}
+            value={formData?.phone || ""}
+          />
+        </FormItemWrapper>
+        <FormItemWrapper className="md:col-span-1">
+          <label
+            className="text-sm font-light lowercase text-stone-600"
+            htmlFor="email"
+          >
+            Email
+          </label>
+
+          <input
+            type="text"
+            name="email"
+            id="email"
+            className="text-bold rounded border border-stone-200 p-2 "
+            onChange={handleChange}
+            value={formData?.email || ""}
+          />
+        </FormItemWrapper>
+        <FormItemWrapper>
+          <label
+            className="text-sm font-light lowercase text-stone-600"
+            htmlFor="website"
+          >
+            Website
+          </label>
+          <input
+            type="text"
+            name="website"
+            className="text-bold rounded border border-stone-200 p-2 "
+            id="website"
+            onChange={handleChange}
+            value={formData?.website || ""}
+          />
+        </FormItemWrapper>
+        <FormItemWrapper className="flex w-full flex-row">
+          <FormItemWrapper className="w-1/2">
+            <label
+              className="text-sm font-light lowercase text-stone-600"
+              htmlFor="address"
+            >
+              Address
+            </label>
+            <input
+              type="text"
+              name="address"
+              className="text-bold rounded border border-stone-200 p-2 "
+              id="address"
+              value={formData?.address || ""}
+              onChange={handleChange}
+            />
+          </FormItemWrapper>
+          <FormItemWrapper className="w-1/6">
+            <label
+              className="text-sm font-light lowercase text-stone-600"
+              htmlFor="city"
+            >
+              City
+            </label>
+            <input
+              type="text"
+              name="city"
+              className="text-bold rounded border border-stone-200 p-2 "
+              id="city"
+              value={formData?.city || ""}
+              onChange={handleChange}
+            />
+          </FormItemWrapper>
+          <FormItemWrapper className="w-1/6">
+            <label
+              className="text-sm font-light lowercase text-stone-600"
+              htmlFor="state"
+            >
+              State
+            </label>
+            <input
+              type="text"
+              name="state"
+              className="text-bold rounded border border-stone-200 p-2 "
+              id="state"
+              value={formData?.state || ""}
+              onChange={handleChange}
+            />
+          </FormItemWrapper>
+          <FormItemWrapper className="w-1/6">
+            <label
+              className="text-sm font-light lowercase text-stone-600"
+              htmlFor="zip"
+            >
+              Zip
+            </label>
+            <input
+              type="text"
+              name="zip"
+              className="text-bold rounded border border-stone-200 p-2 "
+              id="zip"
+              value={formData?.zip || ""}
+              onChange={handleChange}
+            />
+          </FormItemWrapper>
         </FormItemWrapper>
         <FormItemWrapper>
           <label
@@ -259,7 +351,7 @@ export function NewOrganizationForm() {
           />
         </FormItemWrapper>
 
-        <button className="col-span-1  rounded bg-rose-500 p-2 text-white md:col-span-2">
+        <button className="col-span-1  rounded bg-rose-500 p-2 text-white md:col-span-full">
           Submit
         </button>
       </form>
@@ -267,19 +359,25 @@ export function NewOrganizationForm() {
   );
 }
 
-export function NewOrgButton ({className, ...props}: {
-  className?: string,
-  props?: React.ComponentProps<typeof Link>
-} ){
+export function NewOrgButton({
+  className,
+  ...props
+}: {
+  className?: string;
+  props?: React.ComponentProps<typeof Link>;
+}) {
   const admin = useUserStore((state) => state.admin);
-  
-  return (
-    admin ? <Link
+
+  return admin ? (
+    <Link
       {...props}
       href={NEW_ORG_URL}
-      className={twMerge("bg-rose-500 text-white text-center rounded py-2 px-4 text-sm font-bold w-full", className || '') }
+      className={twMerge(
+        "w-full rounded bg-rose-500 px-4 py-2 text-center text-sm font-bold text-white",
+        className || ""
+      )}
     >
       Add New Organization
-    </Link> : null
-  );
+    </Link>
+  ) : null;
 }
