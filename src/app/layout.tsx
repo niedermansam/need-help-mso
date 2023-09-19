288a3f3977d17a30cb4dad47cdc3af4eb91a40cb
Sam Supplee-Niederman <niederman.sam@gmail.com>
2023-09-19 12:56:16 -0600

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import NavBar from "./_components/Nav";
 import { Providers } from "./providers";
@@ -14,6 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="02fa300e-06b5-43d0-a7e7-002ff7deb11e"
+          src="https://umami-sepia.vercel.app/umami.js"
+        ></script>
+      </Head>
       <body className="bg-stone-50">
         <Providers>
           <NavBar />
