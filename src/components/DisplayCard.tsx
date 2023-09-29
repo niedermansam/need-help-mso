97c10764893f57e2cdc27c6e1a988a5e00c12a4f
Sam Supplee-Niederman <niederman.sam@gmail.com>
2023-09-28 19:45:34 -0600

    linting

--- src/components/DisplayCard.tsx
+++ src/components/DisplayCard.tsx
@@ -121,7 +121,7 @@ export function ProgramCard({
         >
           More Info
         </Link>
-        <UpdateProgramModal program={{...program, orgId, tags: program.tags.map(x => x.tag)}} />
+        <UpdateProgramModal program={{...program,  tags: program.tags.map(x => x.tag)}} />
       </div>
     </CardWrapper>
   );
