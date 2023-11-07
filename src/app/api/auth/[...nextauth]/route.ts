f0f59b0fe7962fbb1b1fd7f7486197231a76e9bc
Sam Supplee-Niederman <niederman.sam@gmail.com>
2023-11-07 10:20:46 -0700

    auth on edge

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import { authOptions } from "../../../../server/auth";
 
+export const runtime = "edge";
+
 const handler = NextAuth(authOptions) as undefined;
 
 export { handler as GET, handler as POST };
