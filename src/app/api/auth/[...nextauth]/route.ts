b6a4a6a0bc0ed629b2d178ed147388a18f0dbe92
Sam Supplee-Niederman <niederman.sam@gmail.com>
2024-01-02 13:41:47 -0700

    remove edge

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "../../../../server/auth";
 
-export const runtime = "edge";
-
+ 
 const handler = NextAuth(authOptions) as undefined;
 
 export { handler as GET, handler as POST };
